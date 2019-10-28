61d23cd9e07b2ee86fbc468e05285522a30eb301
asojka09 <austinsojka@gmail.com>
2019-10-28 08:59:59 -0500

    Applied code cleanup

--- MonoGame/NeonPartyGamesController/Activity1.cs
+++ MonoGame/NeonPartyGamesController/Activity1.cs
@@ -2,26 +2,25 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Views;
+using Microsoft.Xna.Framework;
 
 namespace NeonPartyGamesController
 {
-    [Activity(Label = "NeonPartyGamesController"
-        , MainLauncher = true
-        , Icon = "@drawable/icon"
-        , Theme = "@style/Theme.Splash"
-        , AlwaysRetainTaskState = true
-        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
-        , ScreenOrientation = ScreenOrientation.FullUser
-        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout)]
-    public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            var g = new Game1();
-            SetContentView((View)g.Services.GetService(typeof(View)));
-            g.Run();
-        }
-    }
+	[Activity(Label = "NeonPartyGamesController"
+		, MainLauncher = true
+		, Icon = "@drawable/icon"
+		, Theme = "@style/Theme.Splash"
+		, AlwaysRetainTaskState = true
+		, LaunchMode = LaunchMode.SingleInstance
+		, ScreenOrientation = ScreenOrientation.FullUser
+		, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout)]
+	public class Activity1 : AndroidGameActivity
+	{
+		protected override void OnCreate(Bundle bundle) {
+			base.OnCreate(bundle);
+			var g = new Game1();
+			this.SetContentView((View)g.Services.GetService(typeof(View)));
+			g.Run();
+		}
+	}
 }
-
