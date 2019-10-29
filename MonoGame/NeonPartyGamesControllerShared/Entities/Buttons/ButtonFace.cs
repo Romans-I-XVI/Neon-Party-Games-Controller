3e0abebbc8f65934c6178de345c14be74063d79b
asojka09 <austinsojka@gmail.com>
2019-10-29 15:03:18 -0500

    Added ButtonFace

---
+++ MonoGame/NeonPartyGamesControllerShared/Entities/Buttons/ButtonFace.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoEngine;
+using NeonPartyGamesController.Enums;
+using NeonPartyGamesController.Rooms;
+
+namespace NeonPartyGamesController.Entities.Buttons
+{
+	public class ButtonFace : Button
+	{
+		private static Circle _collider_circle => new Circle(0, 0, 60);
+		private readonly Faces Face;
+		private Colors CurrentColor;
+
+		public ButtonFace(Faces face, int x, int y, float scale) : base(x, y, scale, GetSprite(face), _collider_circle, GetOnClick(face)) {
+			this.Face = face;
+			this.CurrentColor = Settings.PlayerColor;
+		}
+
+		public override void onUpdate(float dt) {
+			base.onUpdate(dt);
+			if (this.CurrentColor != Settings.PlayerColor) {
+				this.RemoveSprite("main");
+				this.AddSprite("main", GetSprite(this.Face));
+				this.CurrentColor = Settings.PlayerColor;
+			}
+		}
+
+		public override void onDraw(SpriteBatch sprite_batch) {
+			var region_pupils = SpriteSheetHolder.SpriteSheet.GetRegion("faces/character_pupils_" + (int)Settings.PlayerColor);
+			sprite_batch.Draw(region_pupils, this.Position, Color.White, 0, this.GetSprite("main").Scale);
+			base.onDraw(sprite_batch);
+		}
+
+		private static Sprite GetSprite(Faces face) {
+			var region = SpriteSheetHolder.SpriteSheet.GetRegion("faces/character_" + (int)face + "_" + (int)Settings.PlayerColor);
+			return new Sprite(region);
+		}
+
+		private static Action GetOnClick(Faces face) {
+			return () => Settings.PlayerFace = face;
+		}
+	}
+}
--- MonoGame/NeonPartyGamesControllerShared/Rooms/RoomSelectFace.cs
+++ MonoGame/NeonPartyGamesControllerShared/Rooms/RoomSelectFace.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using MonoEngine;
+using NeonPartyGamesController.Entities;
 using NeonPartyGamesController.Entities.Buttons;
+using NeonPartyGamesController.Enums;
 
 namespace NeonPartyGamesController.Rooms
 {
@@ -8,9 +11,34 @@ namespace NeonPartyGamesController.Rooms
 	{
 		public override void onSwitchTo(Room previous_room, Dictionary<string, object> args) {
 			Engine.SpawnInstance<ButtonBack>();
+			Engine.SpawnInstance<CurrentFaceDrawer>();
+
+			this.SpawnFaces();
 		}
 
 		public override void onSwitchAway(Room next_room) {
+		}
+
+		private void SpawnFaces() {
+			float layout_scale = 1f;
+			float required_space = 1116f;
+			if (Engine.Game.CanvasWidth < required_space)
+				layout_scale = Engine.Game.CanvasWidth / required_space;
+			float scale = 1.33f * layout_scale;
+			int spread = (int)(186 * layout_scale);
+			int start_x = Engine.Game.CanvasWidth / 2;
+			int start_y = 440;
+
+			for (int x = 0; x < 6; x++) {
+				int offset_x = (spread * x + spread / 2) - (spread * 6) / 2;
+				for (int y = 0; y < 2; y++) {
+					int offset_y = y * spread;
+					int face_int = (x + (y * 6)) + 1;
+					Faces face = Enum.Parse<Faces>("Face_" + face_int);
+					var button = new ButtonFace(face, start_x + offset_x, start_y + offset_y, scale);
+					Engine.SpawnInstance(button);
+				}
+			}
 		}
 	}
 }
