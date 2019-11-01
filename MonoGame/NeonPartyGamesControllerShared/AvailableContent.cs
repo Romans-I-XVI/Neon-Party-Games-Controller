43b4d77c11c2436380315f86af6a19b998227a05
Austin Sojka <austinsojka@gmail.com>
2019-11-01 14:37:25 -0500

    Added sounds to AvailableSounds

--- MonoGame/NeonPartyGamesControllerShared/AvailableContent.cs
+++ MonoGame/NeonPartyGamesControllerShared/AvailableContent.cs
@@ -13,5 +13,30 @@ namespace NeonPartyGamesController
 
 	public enum AvailableMusic {}
 
-	public enum AvailableSounds {}
+	public enum AvailableSounds
+	{
+		cell_absorb_mini,
+		cell_booster_grow,
+		cell_booster_magnet_down,
+		cell_booster_magnet_up,
+		cell_booster_shrink_attack,
+		cell_booster_shrink,
+		cell_booster_speed_down,
+		cell_booster_speed_up,
+		cell_death,
+		click,
+		geometry_negative_poof,
+		geometry_poof,
+		hit_custom,
+		lose,
+		puck_hit,
+		puck_other_team_score,
+		puck_team_score,
+		squared_death,
+		sumo_out_of_arena,
+		tag_it,
+		tag_not_it,
+		win
+
+	}
 }
