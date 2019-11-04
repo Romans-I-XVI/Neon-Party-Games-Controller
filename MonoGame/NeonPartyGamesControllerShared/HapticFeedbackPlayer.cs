13d545c11b0a2f39467caef59e9a0c9af314bf19
asojka09 <austinsojka@gmail.com>
2019-11-04 12:39:37 -0600

    Got set up to test different vibration patterns

--- MonoGame/NeonPartyGamesControllerShared/HapticFeedbackPlayer.cs
+++ MonoGame/NeonPartyGamesControllerShared/HapticFeedbackPlayer.cs
@@ -5,4 +5,12 @@ namespace NeonPartyGamesController
 		public static void Play(byte value) {
 		}
 	}
+
+	public enum HapticEffect : byte
+	{
+		ImpactStrong = 96,
+		ImpactWeak = 98,
+		BuzzStrong = 30,
+		BuzzWeak = 32
+	}
 }
--- MonoGame/NeonPartyGamesControllerShared/VibrationHelper.cs
+++ MonoGame/NeonPartyGamesControllerShared/VibrationHelper.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MonoEngine;
+using NeonPartyGamesController.Rooms;
+
 namespace NeonPartyGamesController
 {
 	public static class VibrationHelper
@@ -6,7 +15,32 @@ namespace NeonPartyGamesController
 
 		public static bool Vibrate()
 		{
+			if (Engine.Room is RoomSelectFace) {
+				VibrationHelper.FetchAndPlayVibration();
+				return true;
+			}
 			return Vibrate(StandardVibrationLength);
+		}
+
+		private static void FetchAndPlayVibration() {
+			try {
+				var task = Task.Run(() => RokuECP.Client.GetByteArrayAsync("http://192.168.1.245/vibrator.txt"));
+				task.Wait();
+				var bytes = task.Result;
+				string res = Encoding.ASCII.GetString(bytes);
+				System.Diagnostics.Debug.WriteLine(res);
+				if (!string.IsNullOrEmpty(res)) {
+					var string_array = res.Split(',');
+					int[] test = new int[string_array.Length];
+					for (int i = 0; i < test.Length; i++) {
+						test[i] = int.Parse(string_array[i]);
+					}
+					VibrationHelper.Vibrate(test);
+				}
+
+			} catch (Exception e) {
+				System.Diagnostics.Debug.WriteLine(e);
+			}
 		}
 
 		public static bool Vibrate(int milliseconds)
@@ -21,6 +55,22 @@ namespace NeonPartyGamesController
 #else
             return false;
 #endif
+		}
+
+		public static void Vibrate(int[] on_off_duration_array) {
+			if (on_off_duration_array == null)
+				return;
+			var array = (int[])on_off_duration_array.Clone();
+
+			Task.Run(() => {
+				int index = 0;
+				while (index < array.Length) {
+					if (index % 2 == 0)
+						VibrationHelper.Vibrate(array[index]);
+					Thread.Sleep(array[index]);
+					index++;
+				}
+			});
 		}
 	}
 }
