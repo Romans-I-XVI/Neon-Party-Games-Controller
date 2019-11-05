809c291c50d816837de2dde1a3dabad940a8b082
Austin Sojka <austinsojka@gmail.com>
2019-11-04 18:52:22 -0600

    Added timings for haptic feedback player

--- MonoGame/NeonPartyGamesControllerShared/HapticFeedbackPlayer.cs
+++ MonoGame/NeonPartyGamesControllerShared/HapticFeedbackPlayer.cs
@@ -5,12 +5,22 @@ namespace NeonPartyGamesController
 	public static class HapticFeedbackPlayer
 	{
 		public static Dictionary<HapticEffect, int[]> Haptics = new Dictionary<HapticEffect, int[]> {
-			[HapticEffect.ShortBuzzWeak] = new []{20, 30, 20}
+			[HapticEffect.ImpactStrong] = new[] {32},
+			[HapticEffect.ImpactWeak] = new[] {25},
+			[HapticEffect.ShortBuzzStrong] = new[] {40, 15, 40},
+			[HapticEffect.ShortBuzzWeak] = new[] {20, 30, 20},
+			[HapticEffect.LongBuzzStrong] = new[] {50, 0, 50, 0, 50, 0, 50, 0, 50, 0, 50},
+			[HapticEffect.TripleStrongClick] = new[] {25, 50, 25, 50, 25}
 		};
 
 		public static void Play(byte value) {
-			if (HapticFeedbackPlayer.Haptics.ContainsKey((HapticEffect)value))
-				VibrationHelper.Vibrate(HapticFeedbackPlayer.Haptics[(HapticEffect)value]);
+			if (HapticFeedbackPlayer.Haptics.ContainsKey((HapticEffect)value)) {
+				var on_off_duration_array = HapticFeedbackPlayer.Haptics[(HapticEffect)value];
+				if (on_off_duration_array.Length == 1)
+					VibrationHelper.Vibrate(on_off_duration_array[0]);
+				else
+					VibrationHelper.Vibrate(on_off_duration_array);
+			}
 		}
 	}
 
